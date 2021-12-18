86fef918cb848d53ddb1727001a9cc1f8b259798
용주 곽 <amese1225@gmail.com>
2021-12-18 22:30:32 +0900

    샘플 프로젝트 삭제

--- FirstPlayer/Intermediate/Build/Win64/FirstPlayerEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.28.29914
-#include "D:/Project/UnrealPractice/FirstPlayer/Intermediate/Build/Win64/FirstPlayerEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- FirstPlayer/Source/FirstPlayer/FirstPlayer.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "FirstPlayer.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FirstPlayer, "FirstPlayer" );
- 
