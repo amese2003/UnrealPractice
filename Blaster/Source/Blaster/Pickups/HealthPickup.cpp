73ef3aaf1ac7d86cbdd7401fc4f796610e943247
용주 곽 <amese1225@gmail.com>
2024-08-01 00:20:19 +0900

    Health Pickup

---
+++ Blaster/Source/Blaster/Pickups/HealthPickup.cpp
@@ -0,0 +1,42 @@
+#include "HealthPickup.h"
+#include "Blaster/Character/BlasterCharacter.h"
+#include "Blaster/BlasterComponents/BuffComponent.h"
+#include "NiagaraFunctionLibrary.h"
+#include "NiagaraComponent.h"
+#include UE_INLINE_GENERATED_CPP_BY_NAME(HealthPickup)
+
+AHealthPickup::AHealthPickup()
+{
+	bReplicates = true;
+	PickupEffectComponent = CreateDefaultSubobject<UNiagaraComponent>(TEXT("PickupEffectComponent"));
+	PickupEffectComponent->SetupAttachment(RootComponent);
+
+}
+
+void AHealthPickup::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	Super::OnSphereOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+
+	ABlasterCharacter* BlasterCharacter = Cast<ABlasterCharacter>(OtherActor);
+	if (BlasterCharacter)
+	{
+
+	}
+
+	Destroy();
+}
+
+void AHealthPickup::Destroyed()
+{
+
+	if (PickupEffect)
+	{
+		UNiagaraFunctionLibrary::SpawnSystemAtLocation(
+			this,
+			PickupEffect,
+			GetActorLocation(),
+			GetActorRotation()
+		);
+	}
+	Super::Destroyed();
+}
