11ffaba20607edda4cf09472b6bfa0114bb36655
용주 곽 <amese1225@gmail.com>
2024-08-04 23:58:18 +0900

    Lag Compensation Component

---
+++ Blaster/Source/Blaster/BlasterComponents/LagCompensationComponent.cpp
@@ -0,0 +1,20 @@
+#include "LagCompensationComponent.h"
+#include UE_INLINE_GENERATED_CPP_BY_NAME(LagCompensationComponent)
+
+ULagCompensationComponent::ULagCompensationComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+
+}
+
+void ULagCompensationComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+void ULagCompensationComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+}
