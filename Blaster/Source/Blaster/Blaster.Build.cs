307922aad56682fd19ebcf767ccacfc75d2055a4
용주 곽 <amese1225@gmail.com>
2024-08-10 23:41:43 +0900

    리턴 메인메뉴 버튼

--- Blaster/Source/Blaster/Blaster.Build.cs
+++ Blaster/Source/Blaster/Blaster.Build.cs
@@ -8,7 +8,7 @@ public class Blaster : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "Niagara" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "Niagara", "MultiplayerSessions" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
